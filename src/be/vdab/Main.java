2a9d5cb347de23f56ee96b96d0225cfc18d1628e
Gilles <gillesdebaets@gmail.com>
2014-11-17 14:17:11 +0100

    en ook onder

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo iedereen daar buiten en ook binnen");
+		System.out.println("Hallo iedereen daar buiten en ook binnen en ook onder");
 
 	}
 
